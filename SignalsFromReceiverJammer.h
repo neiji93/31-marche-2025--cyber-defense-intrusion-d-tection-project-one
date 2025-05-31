5b72501f0f52f5d6982220c5bfe96e1e8a1e2885
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-31 15:03:25 +0200

    Create SignalsFromReceiverJammer.h

---
+++ SignalsFromReceiverJammer.h
@@ -0,0 +1,15 @@
+//signals could take months to be received.
+//the imsecondhouston ls (my os) can
+//use slow patching then it is suited for that pruplse.
+//on the other hand, open source physical or mathematical models
+//are suited to receive those signals.
+//bit the simpliest thing to dnis to use mean field games.
+//because last equations we have to mpdel are simple too.
+//then the hard thing to do is the middle range signals datas processing.
+
+class moddlerangesignalsbymeanfieldgzlesproxessor
+{
+public:
+   void chaosinjectstacksonedummydatara'ges...
+
+}
