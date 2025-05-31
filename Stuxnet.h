be3a4a41f51be9e5759117c9305412460ade2cfb
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-31 13:01:03 +0200

    Create Stuxnet.h

---
+++ Stuxnet.h
@@ -0,0 +1,5 @@
+//prevuously, we used several booters for stuxnet.
+//thid was a cyber attack. but now, if we validate userspace and kernel mpdules,
+//we would be able to prevent our os to catch wrong signals.
+//lets start we previous 4 dummy behavioural bots.
+//notice that the botd are well known. the' ise open source models.
